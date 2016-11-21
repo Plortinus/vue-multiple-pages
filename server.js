31c00e5fd9854607638616088bc240cbae945c19
王晓勇 <plortinus@gmail.com>
2016-11-21 14:05:19 +0800

    reset server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var TARGET = 'https://github.com';
+//var TARGET = 'https://github.com';
 var express = require('express');
 var app = express();
 var http = require('http');
@@ -7,13 +7,13 @@ var proxy = httpProxy.createProxyServer({});
 
 app.use(express.static('./dist'));
 
-app.all('/*', function(req, res, next) {
-  console.log(req.url);
-  return proxy.web(req, res, {
-    target: TARGET
-  });
-  // next();
-});
+// app.all('/*', function(req, res, next) {
+//   console.log(req.url);
+//   return proxy.web(req, res, {
+//     target: TARGET
+//   });
+//   // next();
+// });
 
 app.get('/', function(req, res) {
   res.send('Hello Vue');
